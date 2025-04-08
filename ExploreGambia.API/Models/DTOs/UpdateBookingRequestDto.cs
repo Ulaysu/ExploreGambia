84eb72f4a0d57d258535190b66061a01768bbcb7
Sulayman Sanyang <emiress921@gmail.com>
2025-04-08 18:26:58 +0000

    feat(bookings): Enhance booking deletion privacy and update DTO
    This commit focuses on improvements within the bookings feature:
    
    - **Booking Deletion:** The `DeleteBooking` action has been updated to prioritize data privacy. Instead of returning the deleted `Booking` details in the response, it now returns a simple success message upon successful deletion. This avoids potentially exposing sensitive information unnecessarily.
    
    - **Update Booking DTO:** The `TourId` property has been added to the `UpdateBookingRequestDto`. This allows clients to update the associated tour when modifying a booking.

--- ExploreGambia.API/Models/DTOs/UpdateBookingRequestDto.cs
+++ ExploreGambia.API/Models/DTOs/UpdateBookingRequestDto.cs
@@ -5,9 +5,9 @@ namespace ExploreGambia.API.Models.DTOs
 {
     public class UpdateBookingRequestDto
     {
-       // public Guid TourId { get; set; } // Foreign Key (FK) to Tour
+       public Guid TourId { get; set; } // Foreign Key (FK) to Tour
 
-       // public DateTime BookingDate { get; set; } = DateTime.UtcNow; // Timestamp
+        public DateTime BookingDate { get; set; } = DateTime.UtcNow; // Timestamp
         public int NumberOfPeople { get; set; } // Number of participants
 
         
