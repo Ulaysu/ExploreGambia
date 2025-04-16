686aec4c1aeb9f5f6d61987949925cd68cade5b9
Sulayman Sanyang <emiress921@gmail.com>
2025-04-16 19:08:58 +0000

    feat: create a custom Execption class TourGuideNotFoundException.cs

---
+++ ExploreGambia.API/Exceptions/TourGuideNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace ExploreGambia.API.Exceptions
+{
+    public class TourGuideNotFoundException : Exception
+    {
+        public TourGuideNotFoundException(Guid tourGuideId)
+            :base($"Tour Guide with ID {tourGuideId} not found.")
+        {
+            
+        }
+    }
+}
