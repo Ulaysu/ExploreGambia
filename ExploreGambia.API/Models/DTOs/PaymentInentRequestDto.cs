2f1c9983d6c59ab69283e0b76756ea8114909474
Sulayman Sanyang <emiress921@gmail.com>
2026-05-25 20:12:40 +0000

    feat: Created DTO for Payment Intent request to be sent to modem pay API

---
+++ ExploreGambia.API/Models/DTOs/PaymentInentRequestDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExploreGambia.API.Models.DTOs
+{
+    public class PaymentInentRequestDto
+    {
+        [JsonPropertyName("amount")]
+        public decimal Amount { get; set; }
+
+        [JsonPropertyName("currency")]
+        public string Currency { get; set; } = "GMD";
+
+        [JsonPropertyName("return_url")]
+        public string ReturnUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("cancel_url")]
+        public string CancelUrl { get; set; } = string.Empty;
+
+        [JsonPropertyName("metadata")]
+        public Dictionary<string, string> Metadata { get; set; } = new();
+
+        [JsonPropertyName("network")]
+        public string? Network { get; set; }
+
+        [JsonPropertyName("account_number")]
+        public string? AccountNumber { get; set; }
+
+        [JsonPropertyName("skip_url_validation")]
+        public bool SkipUrlValidation { get; set; } = false;
+    }
+}
