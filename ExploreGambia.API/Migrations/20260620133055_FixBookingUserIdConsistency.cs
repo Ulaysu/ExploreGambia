50acc2fefbb6d7c3f722621991eab2ccdb1d6899
Sulayman Sanyang <emiress921@gmail.com>
2026-06-20 13:36:29 +0000

    feat: Clean orphaned UserId references BEFORE FK enforcement

---
+++ ExploreGambia.API/Migrations/20260620133055_FixBookingUserIdConsistency.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExploreGambia.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixBookingUserIdConsistency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Clean orphaned UserId references BEFORE FK enforcement
+            migrationBuilder.Sql(@"
+        UPDATE ""Bookings""
+        SET ""UserId"" = NULL
+        WHERE ""UserId"" IS NOT NULL
+        AND ""UserId"" NOT IN (
+            SELECT ""Id"" FROM ""AspNetUsers""
+        );
+    ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
